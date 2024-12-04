5757a88e37c081d333800bcb9e212c09e2b4fa46
Michael Potishman <sc23mp2@leeds.ac.uk>
2024-12-04 00:21:39 +0000

    what

--- app/static/cookies.js
+++ app/static/cookies.js
@@ -33,35 +33,9 @@ window.addEventListener("load", function() {
         }
     });
 
-    document.getElementById('manage-cookies').addEventListener('click', function() {
-        window.cookieconsent.initialise({
-            "palette": {
-                "popup": {
-                    "background": "#eaf7f7",
-                    "text": "#5c7291"
-                },
-                "button": {
-                    "background": "#56cbdb",
-                    "text": "#ffffff"
-                }
-            },
-            "content": {
-                "message": "This website uses mandatory cookies as well as optional cookies. Choose your preference below.",
-                "dismiss": "Got it",
-                "allow": "Accept all cookies",
-                "deny": "Accept only mandatory cookies",
-                "link": "Learn more",
-                "href": "https://ico.org.uk/for-the-public/online/cookies/"
-            },
-            "type": "opt-in",
-            "layout": "two-button",
-            onStatusChange: function(status) {
-                // Save the cookie consent status in localStorage when the user accepts cookies
-                localStorage.setItem('cookieconsent_status', status);
-            }
-        });
-    });
 
+
+    
     // get the element which has ID = 'theme-toggle' ==> this is the button for changing theme
     const themeOutput = document.getElementById("theme-toggle");
     const savedTheme = localStorage.getItem('theme');
